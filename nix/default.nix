618c958329fb4ae311dea27751f5b1d6972ab7b8
Thomas Nixon <tom@tomn.co.uk>
2023-02-06 12:10:43 +0000

    build: update nixpkgs

--- nix/default.nix
+++ nix/default.nix
@@ -1,7 +1,7 @@
 { pkgs ? import
     (fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/ceaca998029d4b1ac7fd2bee985a3b0f37a786b5.tar.gz";
-      sha256 = "02wa4bw6s800b9inn8hznr56v4v8x3y44sj9lwmkw9zbxzw6mi7s";
+      url = "https://github.com/NixOS/nixpkgs/archive/cff83d5032a21aad4f69bf284e95b5f564f4a54e.tar.gz";
+      sha256 = "0gcm6k52mfazykhs5s4548qxa25sgf3vafb8jy051ayi9i7076wa";
     })
     { }
 }: with pkgs; callPackage ./packages { python = python3; }
