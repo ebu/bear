de9870de6c9f3102dc1dc835ca20ae3488666c7c
Thomas Nixon <tom@tomn.co.uk>
2023-11-17 18:15:55 +0000

    nix: update ear

--- nix/ear.nix
+++ nix/ear.nix
@@ -2,10 +2,6 @@
 python.pkgs.buildPythonPackage rec {
   name = "ear";
   inherit src;
-  propagatedBuildInputs = with python.pkgs; [ numpy scipy six attrs multipledispatch lxml ruamel_yaml setuptools ];
+  propagatedBuildInputs = with python.pkgs; [ numpy scipy six attrs multipledispatch lxml pyyaml setuptools ];
   doCheck = false;
-  postPatch = ''
-    # latest attrs should be fine...
-    sed -i "s/'attrs.*'/'attrs'/" setup.py
-  '';
 }
