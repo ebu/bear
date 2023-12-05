41b562f136f2aa385f8eb53def1ea0f61c198c41
Thomas Nixon <tom@tomn.co.uk>
2023-12-05 17:54:39 +0000

    nix: update to 23.11

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "nixpkgs/nixos-23.05";
+  inputs.nixpkgs.url = "nixpkgs/nixos-23.11";
 
   inputs.ear_src = {
     type = "git";
