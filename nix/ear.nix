251af560d502c977adf55ba1edaf59ddc6a1b222
Thomas Nixon <tom@tomn.co.uk>
2024-04-16 19:24:47 +0100

    nix: update ear

--- nix/ear.nix
+++ nix/ear.nix
@@ -2,6 +2,9 @@
 python.pkgs.buildPythonPackage rec {
   name = "ear";
   inherit src;
-  propagatedBuildInputs = with python.pkgs; [ numpy scipy six attrs multipledispatch lxml pyyaml setuptools ];
+  propagatedBuildInputs = with python.pkgs; [ numpy scipy six attrs multipledispatch lxml pyyaml importlib-resources ];
+  nativeBuildInputs = with python.pkgs; [ setuptools ];
+  pyproject = true;
+
   doCheck = false;
 }
